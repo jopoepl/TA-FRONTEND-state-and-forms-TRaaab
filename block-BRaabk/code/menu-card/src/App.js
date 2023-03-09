c55801b6da934500ca5313cfd2c31a43327bfdde
jopoepl <joel_joseph93@outlook.com>
2023-03-09 18:50:41 +0530

    Push Menu card

---
+++ block-BRaabk/code/menu-card/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import MenuItems from './MenuItems';
+
+function App() {
+  return (
+    <div className="App">
+     <h1 className="menu-title">Our Menu</h1>
+     <MenuItems/>
+    </div>
+  );
+}
+
+export default App;
